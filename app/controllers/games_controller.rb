eb75224c33d17d9e3a2f46719a07d249bd2f569c
Ben Golden <bengolden1@gmail.com>
2017-04-03 11:06:33 -0500

    add page to create new games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,11 +3,16 @@ class GamesController < ApplicationController
   before_action :load_game, only: [:show, :update]
 
   def index
-
+    @game = Game.new
   end
 
   def show
     @squares_under_fire = @game.squares_under_fire    
+  end
+
+  def create
+    @game = Game.create
+    redirect_to game_path(@game.stub)
   end
 
   def update
